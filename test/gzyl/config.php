041dd7e522aa843e411e3aa8006b9b63415e844d
欧阳愧 <1650129775@qq.com>
2022-03-29 16:26:07 +0800

    init

--- test/gzyl/config.php
+++ test/gzyl/config.php
@@ -6,3 +6,11 @@ return [
     'private_key_path'=> 'D:\PhpstormProjects\composer\Payment\test\gzyl\cert\9262001437_rsa_private_key_2048.pem',
     'public_key_path'=> 'D:\PhpstormProjects\composer\Payment\test\gzyl\cert\9262001437_rsa_public_key_2048.pem',
 ];
+
+// return [
+//     'syscode'=> '20000243',
+//     'account'=> '9262001137',
+//     'md5_key'=> '2b78229e1091bc094e1c0b11b94b6ac7',
+//     'private_key_path'=> 'D:\PhpstormProjects\composer\Payment\test\gzyl\cert\rsa_private_key_2048.pem',
+//     'public_key_path'=> 'D:\PhpstormProjects\composer\Payment\test\gzyl\cert\rsa_public_key_2048.pem',
+// ];
