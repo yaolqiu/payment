b2232eddee12a2b123ecb2a1519b2e76ff9f44fa
欧阳愧 <1650129775@qq.com>
2022-03-30 18:14:30 +0800

    删除compose.json type类型

--- test/weixin/pay.php
+++ test/weixin/pay.php
@@ -8,7 +8,7 @@ try {
     $data = [
         'out_trade_no'=> 'PAY'.time(),
         'appid'=> 'wx409b18fe87732520',
-        'amount'=> 100,
+        'amount'=> 1000,
         'notify_url'=> 'https://wcyx.dianxfu.com/noftify',
         'openid'=> 'oDsRMuJiQ9N0KMdMel9Vu47Q_FUc',
         'subject'=> 'testpay', //经测试发现这个字段不能带空格
